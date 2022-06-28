8f6040fb50b3d2430f248c2f1a61a88929c31d54
Vesnushkka <natasha.kochneva@gmail.com>
2022-06-28 16:48:26 +0500

    task committed'

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
-export const swordsman = {
-  name: "мечник",
-  health: 10,
-  level: 2,
-  attack: 80,
-  defence: 40,
+const bowman = {
+  name: "Лучник",
+  type: "Bowman",
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: "Двойной выстрел",
+      icon: "http://...",
+      description: "Двойной выстрел наносит двойной урон",
+    },
+    {
+      id: 9,
+      name: "Нокаутирующий удар",
+      icon: "http://...",
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
 };
-export const sortOrder = ["name", "level"];
-  
-export function sortObj(obj, firstPart) {
-  const secondPart = [];
-  
-  for (const prop in obj) {
-    if (!firstPart.includes(prop)) {
-          secondPart.push(prop);
-    }
-  }
-  secondPart.sort();
-  
-  return [...firstPart, ...secondPart].map((key) => ({ key: key, value: obj[key] }));;
-}
 
-sortObj(swordsman, sortOrder)
+function showCard({special}) {
+  const arr = [];
+
+  for (let i=0; i < special.length; i++) {
+    if (!special[i].description) {
+      special[i].description = 'Описание недоступно';
+    }
+    arr.push(special[i]);
+  }  
+  return arr;
+}
+  
+showCard(bowman);
